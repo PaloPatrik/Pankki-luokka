b99e2e216ac2f00603d1afdc8de2ab21f4b3d677
PaloPatrik <patrikpp06@gmail.com>
2023-04-06 15:36:12 +0300

    olio 6

--- pankkiScript.js
+++ pankkiScript.js
@@ -1,12 +1,27 @@
 var naytaNumero;
 var talletaRahaa;
 var nostaRahaa;
+var muisti = [];
+var korkoPlusLaina = 0;
+
+var tilitehty = false;
+var lainojenMaara = 0;
 
 class Pankki {
-    constructor (tilinumero = "", saldo = 0, historia = []) {
+    constructor (tilinumero = "", saldo = 0, historia = [], laina, korkoProsentti, korko, luottoraja, lainaaJaljella, lainaEnnen, luottoprosentti, nettoTulot) {
         this.tilinumero = String (tilinumero);
         this.saldo = saldo;
         this.historia = historia;
+        this.laina = Number(laina);
+        this.korkoProsentti = Number(korkoProsentti);
+        this.korko = Number(korko);
+
+        this.luottoraja = Number(luottoraja);
+
+        this.lainaaJaljella = Number(lainaaJaljella);
+        this.nettoTulot = Number(nettoTulot);
+        this.lainaEnnen = Number(lainaEnnen);
+        this.luottoprosentti = Number(luottoprosentti);
     }
 
     talleta() {
@@ -19,9 +34,132 @@ class Pankki {
         document.getElementById("saldoEnnen").innerHTML = "Saldosi ennen tapahtumaa oli: " + tarkastus;
 
     }
+    otaLainaa() {
+        console.log("111");
+        if(tilitehty == true){
+            var lainaMaara = Number(prompt("Paljonko otat lainaa?"));
+            console.log(lainaMaara);
+            if(isNaN(lainaMaara < 0)){
+                document.getElementById("lainaTeksti").innerHTML = "Et voi käyttää negatiivisia lukuja!";
+            }else{
+                lainojenMaara++
+                if(lainojenMaara == 1){
+                    this.laina = lainaMaara;
+                }else{
+                    this.laina += lainaMaara;
+                    console.log(this.laina);
+                }
+            }
+            this.korkoProsentti = 10;
+            this.korko = Math.floor(this.laina * this.korkoProsentti / 100);
+            this.lainaEnnen = korkoPlusLaina;
+            korkoPlusLaina = this.korko + this.laina;
+            muisti.push(korkoPlusLaina);
+            console.log(typeof(this.lainaEnnen), typeof(korkoPlusLaina));
+            this.historia.push(aika() + ": Otit lainaa " + lainaMaara + "€. Maksettavaa lainaa ennen lainausta: " + this.lainaEnnen
+                                      + "€. Lainanoton jälkeen: " + korkoPlusLaina);
+        }else if(tilitehty == true){}else{
+            document.getElementById("tilitiedot").innerHTML = "Syötä tilinumerosi";
+        }
+    }
+    lainanMaksu() {
+        if (tilitehty == true) {
+            var maksettuLaina = Number(document.getElementById("lainanMaksu").value)
+            if(isNaN(maksettuLaina) == true){ 
+               console.log("Syötä vain numeroita");
+               document.getElementById("lainanmaksuTeksti").innerHTML = "Syötä vain numeroita";
+            }else{
+                if(maksettuLaina <= -1){ 
+                    document.getElementById("lainanmaksuTeksti").innerHTML = "Älä syötä negatiivisia lukuja";
+                } else{
+                    console.log(this.lainaaJaljella , maksettuLaina ,   this.lainaaJaljella - maksettuLaina);
+                    if(this.lainaaJaljella - maksettuLaina == NaN){
+                        if(maksettuLaina < 0){
+                            document.getElementById("lainanmaksuTeksti").innerHTML = "Lainasi menee miinukselle";
+                        }
+                    }else{
+                        if(this.lainaaJaljella - maksettuLaina <= -1){
+                            document.getElementById("lainanmaksuTeksti").innerHTML = "Lainasi menee miinukselle";
+                        } else {
+                            
+                            this.lainaaJaljella = muisti[0] - maksettuLaina;
+                            muisti.splice([0],1,this.lainaaJaljella )
     
+                            document.getElementById("lainanmaksuTeksti").innerHTML = "Maksoit lainaasi: " + maksettuLaina + "€";
+                            this.korkoPlusLainaClassGlobal = this.lainaaJaljella;
+                            this.historia.push(hankiAika() + ": Maksoit lainaa " + maksettuLaina + "€. Laina ennen maksua: " + this.lainaEnnen + "€. Maksun jälkeen: " + this.lainaaJaljella + "€.")
+                        }
+                    }
+                }
+            }   
+        }
+    }
+    netto(){
+        if (tilitehty == true) {
+            
+            var nettoSumma = Number(prompt("netto"));
+            console.log(nettoSumma);
+
+            if(isNaN(nettoSumma)){
+                document.getElementById("nettoTeksti").innerHTML = "Syötä vain numeroita";
+                console.log("a");
+            } else {
+                if(nettoSumma <0){
+                    
+                    console.log("a");
+                }else{
+                    
+                    this.nettoTulot = nettoSumma;
+                    this.historia.push(aika() + ": ilmoitit nettotulosi olevan: " + this.nettoTulot + "€")
+                    console.log(this.nettoTulot);
+                }
+            }
+        }
+    }
+
+    luotto(netostaTiedot){
+        if(tilitehty == true){
+            var otettuKorkoProsentti = 0;
+            var maxLuotto = 25 * this.nettoTulot / 100;
+            console.log(maxLuotto + " = max");
+            console.log("Voit ottaa luottoa 0-" + maxLuotto + "€");
+            alert("Voit ottaa luottoa 0-" + maxLuotto + "€")    
+            var luottoSumma = Number(prompt("luotto"));
+            console.log(luottoSumma);
+
+            if(isNaN(luottoSumma)){ 
+                
+            } else{
+                if(luottoSumma < 0){
+                    
+                }
+            }
+            
+
+            if(netostaTiedot == false){  
+                document.getElementById("luottoTeksti").innerHTML = "Voit ottaa luottoa:" + 0 + "€ - " + maxLuotto +"€";
+            }else{ 
+                
+                if(luottoSumma > maxLuotto){
+                    
+                } else {
+                    
+                    otettuKorkoProsentti = luottoSumma * 25 / maxLuotto;
+                    this.luottoprosentti = otettuKorkoProsentti
+                    console.log(this.luottoprosentti + "%");
+    
+                    
+                    this.luottoraja = luottoSumma;
+                    
+                    this.historia.push(aika() +  ": Otit luottoa: " + luottoSumma + "€.")
+                }
+            }
+        }
+    }
+
     naytaTiedot() {
         document.getElementById("asiakasTiedot").innerHTML = "Tilinumerosi on:" + " " + this.tilinumero + '<br>' + "Tilin saldo on:" + " " + this.saldo + "€" + '<br>' + "Tilisi historia:";
+        var luottorajaTeksti = 0;
     }
     naytaHistoria() {
         var t = "";
@@ -31,11 +169,13 @@ class Pankki {
         document.getElementById("nosto").innerHTML = "Viimeisin tapahtuma:" + " " + viimeisin;
 
     }
+
 }
 
+
 class Pankki2 extends Pankki{
-    constructor(tilinumero, saldo, historia){
-        super(tilinumero, saldo, historia);
+    constructor(tilinumero, saldo, historia, korkoProsentti, korko, luottoraja, laina, lainaaJaljella, nettoTulot, lainaEnnen, luottoprosentti){
+        super(tilinumero, saldo, historia, korkoProsentti, korko, luottoraja, laina, lainaaJaljella, nettoTulot, lainaEnnen, luottoprosentti);
     }
     tiliNumero() {
         var tNumero = prompt("Mikä on tilinumerosi?");
@@ -43,6 +183,7 @@ class Pankki2 extends Pankki{
         
         if (regex.test(tNumero)) {
             this.tilinumero = tNumero;
+            tilitehty = true;
         } else {
         console.log("Tilinumero on virheellinen");   
         }
